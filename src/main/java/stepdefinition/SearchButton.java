4603c88b67a613b2046f7d4673b65e55d3f55611
Srikanth Palla <sri3012kanth@outlook.com>
2018-03-19 19:11:43 +0530

    Initial Commit

---
+++ src/main/java/stepdefinition/SearchButton.java
@@ -0,0 +1,15 @@
+package stepdefinition;
+
+import org.openqa.selenium.By;
+
+import cucumber.api.java.en.Then;
+import runner.CucumberRunner;
+
+public class SearchButton extends CucumberRunner {
+
+	@Then("^I click search button$")
+	public void searchButton() throws Throwable {
+		driver.findElement(By.name("btnK")).click();
+	}
+
+}
